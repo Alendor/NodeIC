641a24695cf3f660d2605557dfee0ddf513e9da3
sonorks <julivas96@gmail.com>
2018-05-19 10:33:40 -0500

    heroku deploy

--- app.js
+++ app.js
@@ -11,15 +11,6 @@ app.use(methodOverride());
 
 var codebreaker = require('./controllers/codebreaker');
 
-
-// // Example Route
-// var router = express.Router();
-// router.get('/v1/codebreaker', function(req, res) {
-//   res.send("Hello world!");
-// });
-// app.use(router);
-
-
 var cb = express.Router();
 
 cb.route('/adivinarsecreto/:secret')
@@ -29,20 +20,6 @@ cb.route('/setsecreto')
   .post(codebreaker.setSecret);
 
 app.use(cb);
-// // API routes
-// var tvshows = express.Router();
-
-// tvshows.route('/tvshows')
-//   .get(TVShowCtrl.findAllTVShows)
-//   .post(TVShowCtrl.addTVShow);
-
-// tvshows.route('/tvshows/:id')
-//   .get(TVShowCtrl.findById)
-//   .put(TVShowCtrl.updateTVShow)
-//   .delete(TVShowCtrl.deleteTVShow);
-
-// app.use('/api', tvshows);
-
 // Start server
 app.listen(3000, function() {
   console.log("Node server running on http://localhost:3000");
